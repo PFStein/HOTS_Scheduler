e230ee296b666e132eaa668bc88ef991710f7cd6
Patrick Stein <pfstein91@gmail.com>
2015-03-19 15:56:38 -0500

    Created html files
    Created a bunch of new html files and starting to connect the html pages
    together.

--- teams/urls.py
+++ teams/urls.py
@@ -5,15 +5,11 @@ Created on Mar 19, 2015
 '''
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from teams import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
+    (r'^about/', TemplateView.as_view(template_name="about.html"))
 )
-
-
-#     url(r'^(?P<pk>\w{3,50})/$', views.TeamDetailView.as_view(),
-#         name='team_detail'),
-#     url(r'^(?P<pk>\w{3,50})/$', views.PlayerDetailView.as_view(),
-#          name='player_detail'),
--- teams/views.py
+++ teams/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from django.views import generic
+from django.views.generic import TemplateView
 
-from teams.models import Player,Role,Team
-
-class IndexView(generic.ListView):
-    template_name = 'teams/index.html'
+from teams.models import Player,Role,Team,Event,Registrations
+    
+class HomeView(TemplateView):
+    template_name = "home.html"
+    
+class AboutView(TemplateView):
+    template_name = "about.html"
+    
+class Calendar(TemplateView):
+    template_name = "teams/calendar"
+    
+class TeamIndexView(generic.ListView):
+    template_name = 'teams/team_index.html'
     context_object_name = 'active_teams'
 
     def get_queryset(self):
         """Return all of the teams."""
         return Team.objects.all();
+    
+class PlayerIndexView(generic.ListView):
+    template_name = 'teams/player_index.html'
+    context_object_name = 'active_players'
+
+    def get_queryset(self):
+        """Return all of the teams."""
+        return Player.objects.all();
 
 class TeamDetailView(generic.ListView):
     model = Team
@@ -20,7 +35,5 @@ class TeamDetailView(generic.ListView):
 class PlayerDetailView(generic.ListView):
     model = Player
     template_name = 'teams/player_details.html'    
-    
-class CalendarView():
     
 
